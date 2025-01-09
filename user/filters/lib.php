ba2fcf173d914418e2160b167dc84449a0c1916e
datnq <datnquit@gmail.com>
2025-01-09 13:15:59 +0700

    fix search user

--- user/filters/lib.php
+++ user/filters/lib.php
@@ -107,9 +107,9 @@ class user_filtering {
             '',
             null,
             true,
-            [
-                'mform_isexpanded_id_newfilter' => 0,
-            ],
+//            [
+//                'mform_isexpanded_id_newfilter' => 0,
+//            ],
         );
         if ($adddata = $this->_addform->get_data()) {
             // Clear previous filters.
